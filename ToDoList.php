1630e731ad1b58fe0bc988b22a62b356ef193b5a
Abdelrahman Saeed Elhassan <abdosaeedelhassan@gmail.com>
2020-08-24 11:23:36 +0200

    updates

--- ToDoList.php
+++ ToDoList.php
@@ -2,20 +2,30 @@
 
 
 return [
-    '0' => array(
-        'basics' => array(
-            'tablename' => 'Ewallet',
-        ),
-        'fields' => array(
-            '0' => ['user', 'ref', array(
+    '0' => [
+        'basics' => [
+            'tablename' => 'Furniture',
+        ],
+        'fields' => [
+            '0' => ['furnituretypes_id', 'ref', [
+                'tablename' => 'Furnituretypes',
+                'modelname' => 'Furnituretypes',
+                'displayedfield' => 'name',
+            ]],
+            '1' => ['description', 'string', 'nullable'],
+            '2' => ['land_number', 'string', 'nullable'],
+            '3' => ['status', 'integer', 'notnull'],
+            '4' => ['createdby', 'ref', [
                 'tablename' => 'users',
                 'modelname' => 'User',
-                'displayedfield' => 'full_name',
-            )],
-            '1' => ['operation', 'string', 'notnull'],
-            '2' => ['balance', 'string', 'notnull'],
-            '3' => ['shortdesc', 'string', 'notnull'],
-            '4' => ['publish', 'checkbox'],
-        ),
-    ),
+                'displayedfield' => 'name',
+            ]],
+            '5' => ['updatedby', 'ref', [
+                'tablename' => 'users',
+                'modelname' => 'User',
+                'displayedfield' => 'name',
+            ]],
+            '6' => ['published', 'checkbox'],
+        ],
+    ],
 ];
